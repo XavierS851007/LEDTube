118a09cedfd78adebe8b1ef83ee2f4edf599100d
XavierS851007 <sartieauxavier@gmail.com>
2025-11-01 14:02:58 +0100

    Add files via upload

---
+++ pages/584122a1-6ccb-4861-88ef-bb1b9678991c.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-4 py-24">
+    <h3 class="text-center">{{ title }}</h3>
+    <p class="text-center mb-12">{{ description }}</p>
+    <p v-for="paragraph in text" class="text-justify">{{ paragraph }}</p>
+  </div>
+</template>
+<script setup>
+import { inject } from "vue";
+import { getParagraph } from "speech-code";
+const { pid } = defineProps(["pid"]),
+  pages = inject("pages"),
+  { title, description } = pages[pid],
+  text = Array(12).fill().map(() => getParagraph());
+</script>
